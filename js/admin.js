9e2df706d341695804b4ddb3feff59c08b448bc6
oasisofwisdom <oasisofwisdomchurch1@gmail.com>
2026-03-07 10:16:34 +0100

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,58 @@
+import { db } from "./firebase.js"
 
+import {
+doc,
+updateDoc
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js"
+
+let questionIndex = 0
+
+async function nextQuestion(){
+
+questionIndex++
+
+await updateDoc(doc(db,"quiz","state"),{
+
+currentQuestion: questionIndex
+
+})
+
+}
+
+async function prevQuestion(){
+
+questionIndex--
+
+await updateDoc(doc(db,"quiz","state"),{
+
+currentQuestion: questionIndex
+
+})
+
+}
+
+async function startTimer(){
+
+await updateDoc(doc(db,"quiz","state"),{
+
+timer:15
+
+})
+
+}
+
+async function revealAnswer(){
+
+await updateDoc(doc(db,"quiz","state"),{
+
+showAnswer:true
+
+})
+
+}
+
+window.nextQuestion = nextQuestion
+window.prevQuestion = prevQuestion
+window.startTimer = startTimer
+window.revealAnswer = revealAnswer
