eb440b49554587884614ca3ee2552be1c0c5ccf2
oasisofwisdom <oasisofwisdomchurch1@gmail.com>
2026-03-07 10:00:40 +0100

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,15 @@
+document
+.getElementById("loginForm")
+.addEventListener("submit", function(e){
 
+e.preventDefault()
+
+let name = document.getElementById("name").value
+let fellowship = document.getElementById("fellowship").value
+
+localStorage.setItem("bbc_name", name)
+localStorage.setItem("bbc_fellowship", fellowship)
+
+window.location.href = "quiz.html"
+
+})
