f8db1bdcdc5d0de58c7c3edfb9a91806e874370c
oasisofwisdom <oasisofwisdomchurch1@gmail.com>
2026-03-07 10:11:44 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,17 @@
+// Firebase libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js"
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js"
 
+const firebaseConfig = {
+
+apiKey: "AIzaSyAmhkuBBy-jjevVz9AkdMTG3XYzMNTtDlg",
+authDomain: "bbc-quiz-9bbf7.firebaseapp.com",
+projectId: "bbc-quiz-9bbf7",
+
+}
+
+const app = initializeApp(firebaseConfig)
+
+const db = getFirestore(app)
+
+export { db }
